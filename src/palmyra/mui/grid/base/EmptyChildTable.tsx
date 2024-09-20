723408b607de5ca79c86e810de50aadf7243c3b6
surya-ponmani <gsuryaprakash2000@gmail.com>
2024-09-20 12:08:13 +0530

    CSS Changes

--- src/palmyra/mui/grid/base/EmptyChildTable.tsx
+++ src/palmyra/mui/grid/base/EmptyChildTable.tsx
@@ -2,7 +2,7 @@
 const EmptyChild = () => {
   return (
     <div>
-      <div className='empty-child-table-container'>No Data Available</div>
+      <div className='py-empty-child-table-container'>No Data Available</div>
     </div>
   )
 }
