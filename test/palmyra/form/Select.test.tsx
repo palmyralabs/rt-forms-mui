ffc9dc47bfb7ff2ff13684907b23f2552269ddb8
Hemalatha <lhema2093@gmail.com>
2025-04-08 20:44:44 +0530

    IntegerField, Select test cases and MuiIntegerField changes

--- test/palmyra/form/Select.test.tsx
+++ test/palmyra/form/Select.test.tsx
@@ -1,15 +1,16 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { fireEvent, queryByAttribute, render, renderHook } from '@testing-library/react';
 import { MuiSelect } from "../../../src/palmyra";
-import { IForm, IInputField, PalmyraForm } from "@palmyralabs/rt-forms";
-import { useRef } from "react";
+import { IForm, IInputField, ISelectField, PalmyraForm } from "@palmyralabs/rt-forms";
+import React, { act, useRef } from "react";
+import { testOptional2Mandatory } from "./commons/util";
 
 describe('Mui Select', () => {
 
     const initProps = () => {
         const getById = queryByAttribute.bind(null, 'id');
         const formRef = renderHook(() => useRef<IForm>()).result.current;
-        const fieldRef = renderHook(() => useRef<IInputField>()).result.current;
+        const fieldRef = renderHook(() => useRef<ISelectField>()).result.current;
         const getByRole = queryByAttribute.bind(null, 'role')
         // const getByText = queryByAttribute.bind(null, 'text')
         return { getById, formRef, fieldRef, getByRole }
@@ -30,26 +31,26 @@ describe('Mui Select', () => {
         fireEvent.click(selectField);
     });
 
-    // test('Select Change', () => {
-    //     const { formRef, getByRole } = initProps();
-    //     const selectDefn = (
-    //         <PalmyraForm formData={{ select: 1 }} ref={formRef}>
-    //             <MuiSelect id="select" attribute="select" options={{ 1: "True", 2: "False" }} />
-    //         </PalmyraForm>
-    //     );
+    test('Select Change', async () => {
+        const { getById, formRef } = initProps();
+        const selectDefn = (
+            <PalmyraForm formData={{ select: 1 }} ref={formRef}>
+                <MuiSelect id="select" attribute="select" options={{ 1: "True", 2: "False" }} />
+            </PalmyraForm>
+        );
 
-    //     const dom = render(selectDefn);
+        const { getByText } = render(selectDefn);
 
-    //     const selectField = getByRole(dom.container, 'combobox');
+        const dom = render(selectDefn);
 
-    //     fireEvent.mouseDown(selectField);
-    //     // const option = getByText(dom.container, 'False');
-    //     // fireEvent.click(option);
+        const selectField = getById(dom.container, 'select');
 
-    //     expect(selectField).toBeTruthy();
-    // });
+        fireEvent.mouseDown(selectField);
+
+        const option = getByText('False');
+        fireEvent.click(option);
+
+        expect(selectField.textContent).toBe('False');
+    });
 
 });
-
-
-
